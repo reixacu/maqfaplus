1f905feb078cb953599e28b88a3be679370e71c6
XMG7 <reixacu@gmail.com>
2017-01-08 00:27:21 +0100

    Test integració2

--- pages/scriptAfegirFeina.php
+++ pages/scriptAfegirFeina.php
@@ -1,9 +1,10 @@
 <?php
-$idClient = $_GET["id"];
+$idClient = $_POST["id"];
+$descripcio = $_POST["descripcio"];
 
 include "mysql.php";
 include "funcions.php";
-$sql = "INSERT INTO `feines` (`id_feina`, `estat_feina`, `id_client_feina`, `inci_feina`, `acabament_feina`, `facturacio_feina`, `descripcio_feina`) VALUES (NULL, '1', '$idClient', CURDATE(), '2000-01-01', '2000-01-01', ". $_GET["descripcio"] .");";
+$sql = "INSERT INTO `feines` (`id_feina`, `estat_feina`, `id_client_feina`, `inci_feina`, `acabament_feina`, `facturacio_feina`, `descripcio_feina`) VALUES (NULL, '1', '$idClient', CURDATE(), '2000-01-01', '2000-01-01', ". $descripcio .");";
 if ($conn->query($sql) === TRUE) {
     echo "
                             <script>
