4a732603c268a7372675b4c7329b3ded15acb9aa
Klanc <clank201@gmail.com>
2017-01-08 00:37:39 +0100

    Afegir descripcio feina FIX

--- pages/scriptAfegirFeina.php
+++ pages/scriptAfegirFeina.php
@@ -4,7 +4,7 @@ $descripcio = $_POST["descripcio"];
 
 include "mysql.php";
 include "funcions.php";
-$sql = "INSERT INTO `feines` (`id_feina`, `estat_feina`, `id_client_feina`, `inci_feina`, `acabament_feina`, `facturacio_feina`, `descripcio_feina`) VALUES (NULL, '1', '$idClient', CURDATE(), '2000-01-01', '2000-01-01', ". $descripcio .");";
+$sql = "INSERT INTO `feines` (`id_feina`, `estat_feina`, `id_client_feina`, `inci_feina`, `acabament_feina`, `facturacio_feina`, `descripcio_feina`) VALUES (NULL, '1', '$idClient', CURDATE(), '2000-01-01', '2000-01-01', \"". $descripcio ."\");";
 if ($conn->query($sql) === TRUE) {
     echo "
                             <script>
