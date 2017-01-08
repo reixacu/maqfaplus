96822542001558fccb22cb4bd4d1580241b36bc1
XMG7 <reixacu@gmail.com>
2017-01-08 15:12:25 +0100

    Optimazed

--- pages/funcionsFactures.php
+++ pages/funcionsFactures.php
@@ -320,7 +320,6 @@ function printDataVencimentFactura($idFactura)
             }
             else {
               $data = date_modify(date_create($data), '+1 month');
-              $data = date_create($data);
               $mes = date("d", strtotime($data));
               $any = date("Y", strtotime($data));
               echo $data->format($any.'-'.$mes.'-'.$dia1);
