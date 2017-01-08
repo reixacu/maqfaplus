7d5ed24f10fe0defdf535797c1e75e8f70bf7e13
XMG7 <reixacu@gmail.com>
2017-01-08 15:10:47 +0100

    Improved performance

--- pages/funcionsFactures.php
+++ pages/funcionsFactures.php
@@ -320,6 +320,7 @@ function printDataVencimentFactura($idFactura)
             }
             else {
               $data = date_modify(date_create($data), '+1 month');
+              $data = date_create($data);
               $mes = date("d", strtotime($data));
               $any = date("Y", strtotime($data));
               echo $data->format($any.'-'.$mes.'-'.$dia1);
