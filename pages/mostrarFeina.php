031b76aeb8554ce56a1f64067d5067f968c0fa45
Klanc <clank201@gmail.com>
2017-01-08 13:50:01 +0100

    :3

--- pages/mostrarFeina.php
+++ pages/mostrarFeina.php
@@ -65,7 +65,7 @@
             <!-- /.row -->
             <div class=\"row\">
                 <div class=\"col-lg-12\">
-                    <h1 class=\"page-header\"><i class=\"fa fa-tasks\"></i> Feina #". $row["id_feina"] . " | " . $row["descripcio_feina"] . " <link href=\"modificarDescripcioFeina.php\" class=\"fa fa-pencil\"></link></h1> 
+                    <h1 class=\"page-header\"><i class=\"fa fa-tasks\"></i> Feina #". $row["id_feina"] . " | " . $row["descripcio_feina"] . " <form action='modificarDescripcioFeina.php'><button style='margin: 5px;' type='submit' class=\"btn btn-primary\"><i class=\"fa fa-pencil\"></i></button></form></h1> 
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
