6dc58cc6b504ea063c5d71ca5c1d32e68d804cb4
XMG7 <reixacu@gmail.com>
2017-01-08 14:25:19 +0100

    debug

--- pages/funcionsFactures.php
+++ pages/funcionsFactures.php
@@ -303,7 +303,7 @@ function printDataVencimentFactura($idFactura)
               $dia1 = $dia2;
               $dia2 = $temp;
             }
-            $diames = date("d", $data);
+            $diames = date("d-m", $data);
             if ($diames <= $dia2)
             {
               echo "DATADATADATA".$data;
