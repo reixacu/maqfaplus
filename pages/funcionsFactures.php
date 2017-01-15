3fd97b4e5f70db425c6320369b4dd037ed2bd677
Jordi Reixach hiditec <reixacu@gmail.com>
2017-01-15 13:03:22 +0100

    faltava una línia :+1:

--- pages/funcionsFactures.php
+++ pages/funcionsFactures.php
@@ -604,6 +604,7 @@ function mostrarNumeroFactura($numDB) {
     $result = $conn->query($sql);
     if ($result->num_rows > 0)
     {
+      $row = $result->fetch_assoc();
       echo $row[0];
     }
   }
