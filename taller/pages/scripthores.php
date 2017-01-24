23313de458f4fb95deb273b071e217f39a747918
XMG7 <reixacu@gmail.com>
2017-01-24 09:39:33 +0100

    No es guarden hores si no s'entren. Es guarda camp feinaAcabada

--- taller/pages/scripthores.php
+++ taller/pages/scripthores.php
@@ -31,15 +31,18 @@ $conn->query($sql);
 
 for( $i = 0 ; $i< $count ; $i++)
 {
-	if($array[$i]['Feina']!=-1){
+	if($array[$i]['Hores']==""){
 		echo " Feina: " . $array[$i]['Feina'];//
 		echo " Dia: " . $array[$i]['Dia'];//
 		echo " Hores: " . $array[$i]['Hores'];//
 		echo " Detall: " . $array[$i]['Detall'];//
 		echo " Acabada: " . $array[$i]['Acabada']." \n"; //s'ha de posar a algun lloc que encara no existeix
+		$esAcabadaB = $array[$i]['Acabada'];
+		$esAcabada = 0;
+		if ($esAcabadaB) $esAcabada = 1;
 
-		$sql="INSERT INTO `maqfaplus`.`hores` (`id_hores`, `id_treballador_hores`, `id_feina_hores`, `hores_hores`, `detall_hores`, `dia_hores`, `dia_creacio_hores`, `timestamp_hores`) ";
-		$sql=$sql."VALUES (NULL, '".$_GET["idtreballador"]."', '".$array[$i]['Feina']."', '".$array[$i]['Hores']."', '".$array[$i]['Detall']."','".$array[$i]['Dia']."', CURRENT_DATE, CURRENT_TIMESTAMP);";
+		$sql="INSERT INTO `maqfaplus`.`hores` (`id_hores`, `id_treballador_hores`, `id_feina_hores`, `hores_hores`, `detall_hores`, `dia_hores`, `dia_creacio_hores`, `timestamp_hores`, `feina_acabada_hores`) ";
+		$sql=$sql."VALUES (NULL, '".$_GET["idtreballador"]."', '".$array[$i]['Feina']."', '".$array[$i]['Hores']."', '".$array[$i]['Detall']."','".$array[$i]['Dia']."', CURRENT_DATE, CURRENT_TIMESTAMP, $esAcabada);";
 		$result = $conn->query($sql);
 	}
 }
