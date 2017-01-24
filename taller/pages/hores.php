a683217e15472fcdcef29510d1f8a747e9ff0fab
XMG7 <reixacu@gmail.com>
2017-01-24 12:06:05 +0100

    Bug mostrar

--- taller/pages/hores.php
+++ taller/pages/hores.php
@@ -214,7 +214,7 @@ function printNewLines($idTreballador)
         echo"</td>
         <td class=\"dia\"><input type=\"date\" value=\"". $row["dia_hores"] ."\" class=\"form-control\" placeholder=\"Descripció\"></td>
         <td class=\"hores\"><input id=\"hores\" class=\"form-control\" placeholder=\"h\" value=". $row["hores_hores"] ."></td>
-        <td class=\"desc\"><input class=\"form-control\" placeholder=\"Descripció de la feina\" value=".$row["detall_hores"]."></td>
+        <td class=\"desc\"><input class=\"form-control\" placeholder=\"Descripció de la feina\" value=\"".$row["detall_hores"]."\"></td>
         <td class=\"text-center\"><input type=\"checkbox\" class=\"js-switch\" />
     </tr>";
 	}
