46e5771be61d0d966e657e009b96ae37929215ca
XMG7 <reixacu@gmail.com>
2017-01-30 11:21:37 +0100

    Volem accents

--- taller/pages/hores.php
+++ taller/pages/hores.php
@@ -45,7 +45,7 @@
     <script type="text/javascript">
 /* code from qodo.co.uk */
 // create as many regular expressions here as you need:
-var digitsOnly = /[1234567890 A-Za-z\.\,]/g;
+var digitsOnly = /[1234567890 A-Za-z\.\,\à\è\é\í\ò\ó\ù]/g;
 var integerOnly = /[0-9\.]/g;
 var alphaOnly = /[A-Za-z]/g;
 
