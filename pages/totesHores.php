81dfc466ac70c80f193cd4b6d4715aa5b90c70eb
Jordi Reixach <reixacu@gmail.com>
2017-04-05 17:19:24 +0200

    Arreglos interficie totesHores i acceptar GET amb ID per filtrar

--- pages/totesHores.php
+++ pages/totesHores.php
@@ -39,19 +39,27 @@
     <?php
     include "funcionsHores.php";
     include "menu.php";
+    if ($_GET["id"] != NULL)
+    {
+        $id = $_GET["id"];
+    }
+    else {
+      $id = 0;
+    }
     ?>
     <?php
     $sql = "SELECT * FROM `hores` ORDER BY `hores`.`dia_hores` DESC";
     echo "
     <div id=\"page-wrapper\">
         <div class=\"row\">
-            <div class=\"col-lg-12\">
-                <table cellpadding=\"10\">
-                    <tr>
-                        <td><h1 class=\"page-header\"><i class=\"fa fa-globe\"></i> Totes les factures</h1></td>
-                        <td><form class=\"page-header\" action='afegirFactura.php'> <button style=\"margin-top: 5px; margin-left: 15px\" type='submit' class=\"btn btn-primary \"><i class=\"fa fa-plus\"></i> Afegir una factura</button></form></td>
-                    </tr>
-                </table>
+            <div class=\"col-lg-12\">";
+                    if ($id==0)
+                    {
+                        echo "<td><h1 class=\"page-header\"><i class=\"fa fa-globe\"></i> Totes les hores</h1></td>";
+                        else{
+                          echo "<td><h1 class=\"page-header\"><i class=\"fa fa-globe\"></i> Hores ". function getNomTreballador ($id) ."</h1></td>"
+                        }
+                        echo "
             </div>
             <!-- /.col-lg-12 -->
         </div>
@@ -65,7 +73,7 @@
                     <!-- /.panel-heading -->
                     <div class=\"panel-body\">
                         ";
-                        mostrarHores($sql, 1);
+                        mostrarHores($sql, $id);
                     echo "
                     </div>
                 </div>
