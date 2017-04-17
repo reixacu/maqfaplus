a96bc33f601b36eb0980121cb4d1c7a59d9eec1e
Jordi Reixach <reixacu@gmail.com>
2017-04-17 10:31:05 +0200

    Disseny modal selector clients

--- pages/totesFactures.php
+++ pages/totesFactures.php
@@ -90,7 +90,9 @@
                     <div class=\"panel-body\">
                       <div class=\"row\">
                         <div class=\"col-lg-6\">
-                          TEST
+                          ";
+                          printModalClient($desde, $fins);
+                          echo "
                         </div>
                         <div class=\"col-lg-6\">
                           ";
@@ -264,5 +266,34 @@ function printFiltreDataForm($desde, $fins, $idClient)
     <button type=\"submit\" class=\"btn btn-primary btn-sm\"><i class=\"fa fa-search\" aria-hidden=\"true\"></i> Filtrar data</button>
   </form>
   ";
+}
+function printModalClient($desde, $fins)
+{
+  echo "
+                            <button class=\"btn btn-primary btn-sm\" data-toggle=\"modal\" data-target=\"#myModal\">
+                                  Triar Client
+                              </button>
+                              <!-- Modal -->
+                              <div class=\"modal fade\" id=\"myModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
+                                  <div class=\"modal-dialog\">
+                                      <div class=\"modal-content\">
+                                          <div class=\"modal-header\">
+                                              <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
+                                              <h4 class=\"modal-title\" id=\"myModalLabel\">Modal title</h4>
+                                          </div>
+                                          <div class=\"modal-body\">
+                                              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                          </div>
+                                          <div class=\"modal-footer\">
+                                              <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
+                                              <button type=\"button\" class=\"btn btn-primary\">Save changes</button>
+                                          </div>
+                                      </div>
+                                      <!-- /.modal-content -->
+                                  </div>
+                                  <!-- /.modal-dialog -->
+                              </div>
+                              <!-- /.modal -->
+  "
 }
  ?>
