1fe6b3a31789a0c590e927b3d37aa4f4927e1ec5
Jordi Reixach <reixacu@gmail.com>
2017-04-17 10:33:14 +0200

    Modal més ample

--- pages/totesFactures.php
+++ pages/totesFactures.php
@@ -275,7 +275,7 @@ function printModalClient($desde, $fins)
                               </button>
                               <!-- Modal -->
                               <div class=\"modal fade\" id=\"myModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
-                                  <div class=\"modal-dialog\">
+                                  <div class=\"modal-dialog modal-lg\">
                                       <div class=\"modal-content\">
                                           <div class=\"modal-header\">
                                               <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
