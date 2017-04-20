4f8d571f6f2be908b8eff43eabf7bba576878721
Jordi Reixach <reixacu@gmail.com>
2017-04-20 11:58:20 +0200

    Digoyo

--- pages/mostrarAlbara.php
+++ pages/mostrarAlbara.php
@@ -101,6 +101,7 @@
                 printRowDetallsAlbara($id);
 
             echo "
+              <div class=\"row\">
                 <div class=\"col-lg-12\">
                     <div class=\"panel panel-default\">
                         <div class=\"panel-heading\">
