4ea4ff8cf0038047a065015d2304ea6dd7c7e02e
Jordi Reixach <reixacu@gmail.com>
2017-04-20 13:57:34 +0200

    missing include

--- pages/scriptAlbaraNovaFactura.php
+++ pages/scriptAlbaraNovaFactura.php
@@ -1,5 +1,6 @@
 <?php
-
+include "mysql.php";
+include "funcions.php";
 $idAlbara = $_GET["id"];
 $result = getAlbaraData($idAlbara);
 $row = $result->fetch_assoc();
@@ -8,8 +9,7 @@ $row = $result->fetch_assoc();
 $idClient = $row["id_client"];
 //$descripcio = $_POST["descripcio"];
 
-include "mysql.php";
-include "funcions.php";
+
 $sql = "INSERT INTO `factures` (`id_factura`, `numero_factura`, `data_factura`, `data_venciment_factura`, `pagament_realitzat_factura`, `comentari_factura`, `id_client_factura`, `iva_factura`, `base_imposable_factura`) VALUES (NULL, '', '2000-01-01', '2000-01-01', '0', '', '$idClient', '21', '0');";
 if ($conn->query($sql) === TRUE) {
   $resultDadesFactura = getLiniesAlbaraData($idAlbara);
