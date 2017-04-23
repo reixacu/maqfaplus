2694a1290a208cfbd726bd69f0ac40749dfef937
Jordi Reixach <reixacu@gmail.com>
2017-04-24 00:40:22 +0200

    Ara es pot fer '. Fuck cané and his SQL ingection

--- pages/scriptAlbaraNovaFactura.php
+++ pages/scriptAlbaraNovaFactura.php
@@ -27,7 +27,7 @@ if ($conn->query($sql) === TRUE) {
   	$rowDadesFactura = $resultDadesFactura->fetch_assoc();
   		// Tipus de línia amb preu quantitat i preu total
       include "mysql.php";
-      $sql11 = "INSERT INTO `detalls_factures` (`id_df`, `id_factura_df`, `descripcio_df`, `unitats_df`, `preu_unitat_df`) VALUES (NULL, ".getLastFacturaId().", '". $rowDadesFactura['descripcio_df']."', ". $rowDadesFactura['unitats_df'].", ". $rowDadesFactura['preu_unitat_df'].")";
+      $sql11 = "INSERT INTO `detalls_factures` (`id_df`, `id_factura_df`, `descripcio_df`, `unitats_df`, `preu_unitat_df`) VALUES (NULL, ".getLastFacturaId().", `". $rowDadesFactura['descripcio_df']."`, ". $rowDadesFactura['unitats_df'].", ". $rowDadesFactura['preu_unitat_df'].")";
 
       if ($conn->query($sql11) === TRUE) {
       } else {
