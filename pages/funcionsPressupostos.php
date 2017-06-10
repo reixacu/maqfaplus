ea7fd54795138a5db70032762bcc809bfcd605f5
misterchef102 <marc00077@hotmail.com>
2017-06-11 01:29:43 +0200

    Revert "bigger is better"
    This reverts commit 6e97b86799783d3f5adbe58d68944914a5bac255.

--- pages/funcionsPressupostos.php
+++ pages/funcionsPressupostos.php
@@ -103,7 +103,7 @@ function printEstatPressupostColum($id)
               </div>
               <!-- /.modal -->
               <div class=\"modal fade\" id=\"modal1\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"modal1\" aria-hidden=\"true\">
-                  <div class=\"modal-dialog\">
+                  <div class=\"modal-dialog modal-sm\">
                       <div class=\"modal-content\">
                           <div class=\"modal-header\">
                               <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
