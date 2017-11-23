ef663fd5e6f52796af04276dcf44e49f9de5c935
Jordi Reixach <reixacu@gmail.com>
2017-11-23 15:56:43 +0100

    upeo

--- pages/generarFacturaPDF.php
+++ pages/generarFacturaPDF.php
@@ -211,7 +211,7 @@ $pdf->lastPage();
 // ---------------------------------------------------------
 
 //Close and output PDF document
-$pdf->Output( '/home/reixacu/factura_' . $idFactura . '.pdf', 'F');
+$pdf->Output( '/home/pi/factures/factura_' . $idFactura . '.pdf', 'F');
 $pdf->Output( 'factura_' . $numFactura . '.pdf', 'I');
 
 //============================================================+
