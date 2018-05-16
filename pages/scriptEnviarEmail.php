adf0080ae1f7b389848567cb800154582af9a34a
Jordi Reixach <reixacu@gmail.com>
2018-05-16 23:08:50 +0200

    PROD!

--- pages/scriptEnviarEmail.php
+++ pages/scriptEnviarEmail.php
@@ -23,18 +23,7 @@ $mail->From = 'xevifaco@maqfa.cat';
 $mail->FromName = 'Maqfa';
 $mail->addAddress($email);     // Add a recipient
 $mail->addBCC('reixacu@gmail.com');
-//$mail->addBCC('xevifaco@maqfa.cat');
-
-
-
-
-
-// OOJOJOJOJO
-//mail_enviat_factura
-
-
-
-
+$mail->addBCC('xevifaco@maqfa.cat');
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 $mail->addAttachment($ruta);         // Add attachments
